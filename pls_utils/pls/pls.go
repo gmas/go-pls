1063a1ec5b5b602a4907ee216e7b9560e0bd0259
George Masgras <gmasgras@gmail.com>
2015-08-30 20:03:48 -0700

    move PLS logic into into pls package refactor playlist merging

---
+++ pls_utils/pls/pls.go
@@ -0,0 +1,96 @@
+package pls
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Playlist struct {
+	Entries []PlaylistEntry
+	Version string
+}
+
+type PlaylistEntry struct {
+	File  string
+	Title string
+}
+
+func (pl Playlist) Length() int {
+	return len(pl.Entries)
+}
+
+func (plEntry PlaylistEntry) String() string {
+	return fmt.Sprintf("Title: %s\nFile: %s \n", plEntry.Title, plEntry.File)
+}
+
+func (plEntry *PlaylistEntry) Empty() bool {
+	if plEntry.Title == "" && plEntry.File == "" {
+		return true
+	}
+	return false
+}
+
+func Parse(contents string) (pl Playlist, err error) {
+	//FIXME entries needs to grow itself
+	// use a Set so we get no dupes ?
+	_entries := make([]PlaylistEntry, 1024)
+
+	fileRegexp := regexp.MustCompile(`File(\d+)=`)
+	for _, line := range strings.Split(contents, "\n") {
+		fileMatches := fileRegexp.FindStringSubmatch(line)
+		if len(fileMatches) > 0 {
+			fileId, _ := strconv.ParseInt(fileMatches[1], 10, 64)
+			file := strings.TrimPrefix(line, fileMatches[0])
+			(_entries[int64(fileId)]).File = file
+		}
+
+		titleRegexp := regexp.MustCompile(`Title(\d+)=`)
+		titleMatches := titleRegexp.FindStringSubmatch(line)
+		if len(titleMatches) > 0 {
+			titleId, _ := strconv.ParseInt(titleMatches[1], 10, 64)
+			title := strings.TrimPrefix(line, titleMatches[0])
+			(_entries[int32(titleId)]).Title = title
+		}
+	}
+
+	for i := 0; i < len(_entries); i++ {
+		if entry := _entries[i]; !entry.Empty() {
+			pl.Entries = append(pl.Entries, entry)
+		}
+	}
+
+	return pl, nil
+}
+
+func (pl *Playlist) ToPls() string {
+	var buff bytes.Buffer
+
+	buff.WriteString("[playlist]\n")
+	buff.WriteString(fmt.Sprintf("numberofentries=%d\n", len(pl.Entries)))
+	for i, _ := range pl.Entries {
+		idx := i + 1
+		buff.WriteString(fmt.Sprintf(fmt.Sprintf("File%d=%s\n", idx, pl.Entries[i].File)))
+		buff.WriteString(fmt.Sprintf(fmt.Sprintf("Title%d=%s\n", idx, pl.Entries[i].Title)))
+		buff.WriteString(fmt.Sprintf(fmt.Sprintf("Length%d=-1\n", idx)))
+	}
+	buff.WriteString("Version=2\n")
+
+	return buff.String()
+}
+
+func (pl *Playlist) AddEntry(entries ...PlaylistEntry) (int, error) {
+	pl.Entries = append(pl.Entries, entries...)
+	return len(pl.Entries), nil
+}
+
+func (pl *Playlist) Merge(playlists ...Playlist) (Playlist, error) {
+	newPl := Playlist{}
+	newPl.Entries = append(newPl.Entries, pl.Entries...)
+	for i, _ := range playlists {
+		newPl.AddEntry(playlists[i].Entries...)
+	}
+	return newPl, nil
+}
--- pls_utils/plsutil.go
+++ pls_utils/plsutil.go
@@ -1,34 +1,15 @@
 package main
 
 import (
-	"bufio"
+	"./pls"
 	"fmt"
 	"log"
 	"os"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
-type playlistEntry struct {
-	File  string
-	Title string
-	//Length  uint32
-}
-
-func (p *playlistEntry) String() string {
-	return fmt.Sprintf("Title: %s, File: %s \n", p.Title, p.File)
-}
-
-func (p *playlistEntry) Empty() bool {
-	if p.Title == "" && p.File == "" {
-		return true
-	}
-	return false
-}
-
 func main() {
-	var entries []playlistEntry
+	var playlists []pls.Playlist
+
 	argsLen := len(os.Args)
 	//fmt.Printf("args: %d\n", argsLen)
 
@@ -37,90 +18,52 @@ func main() {
 		os.Exit(1)
 	}
 
-	inputs := os.Args[1 : argsLen-1]
-	outFile := os.Args[argsLen-1]
+	inputs := os.Args[1:argsLen]
+	//outFile := os.Args[argsLen-1]
 
 	for _, inputFile := range inputs {
-		readEntries, err := parsePls(inputFile)
+		//TODO make it work with bufio ?
+		contents, err := loadPls(inputFile)
 		if err != nil {
-			log.Printf("%s \n", err)
+			log.Printf("WARNING\t %s", err)
 			continue
 		}
-		for i := 0; i < len(readEntries); i++ {
-			if entry := readEntries[i]; !entry.Empty() {
-				entries = append(entries, entry)
-			}
+
+		playlist, err := pls.Parse(contents)
+		//FIXME extract the error handling for warnings/errors
+		if err != nil {
+			log.Printf("WARNING\t %s", err)
+			continue
 		}
+
+		playlists = append(playlists, playlist)
 	}
-	writePls(entries, outFile)
-}
 
-func writePls(entries []playlistEntry, fileName string) bool {
-	file, err := os.Create(fileName)
-	writer := bufio.NewWriter(file)
-	defer func() {
-		if err := writer.Flush(); err != nil {
-			panic(err)
-		}
-		file.Close()
-	}()
-
+	pl := pls.Playlist{}
+	newPls, err := pl.Merge(playlists...)
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Fprint(writer, "[playlist]\n")
-	fmt.Fprintf(writer, fmt.Sprintf("numberofentries=%d\n", len(entries)))
-	for i, _ := range entries {
-		idx := i + 1
-		fmt.Fprintf(writer, fmt.Sprintf("File%d=%s\n", idx, entries[i].File))
-		fmt.Fprintf(writer, fmt.Sprintf("Title%d=%s\n", idx, entries[i].Title))
-		fmt.Fprintf(writer, fmt.Sprintf("Length%d=-1\n", idx))
-	}
-	fmt.Fprintln(writer, "Version=2")
-
-	return true
+	fmt.Print(newPls.ToPls())
 }
 
-func parsePls(inputPlsFile string) (parsedEntries []playlistEntry, err error) {
-	log.Printf("Reading %s, ", inputPlsFile)
-	contents, err := readLocalFile(inputPlsFile)
-	if err != nil {
-		return nil, fmt.Errorf("%s \n", err)
+//TODO add option to download from URL
+func loadPls(inputPls string) (_ string, _ error) {
+	contents, _err := readLocalFile(inputPls)
+	if _err != nil {
+		return "", fmt.Errorf("%s \n", _err)
 	}
-
-	//FIXME entries needs to grow itself
-	entries := make([]playlistEntry, 1024)
-
-	fileRegexp := regexp.MustCompile(`File(\d+)=`)
-	for _, line := range strings.Split(contents, "\n") {
-		fileMatches := fileRegexp.FindStringSubmatch(line)
-		if len(fileMatches) > 0 {
-			fileId, _ := strconv.ParseInt(fileMatches[1], 10, 64)
-			file := strings.TrimPrefix(line, fileMatches[0])
-			(entries[int32(fileId)]).File = file
-		}
-
-		titleRegexp := regexp.MustCompile(`Title(\d+)=`)
-		titleMatches := titleRegexp.FindStringSubmatch(line)
-		if len(titleMatches) > 0 {
-			titleId, _ := strconv.ParseInt(titleMatches[1], 10, 64)
-			title := strings.TrimPrefix(line, titleMatches[0])
-			(entries[int32(titleId)]).Title = title
-		}
-	}
-	//... lets you pass multiple arguments to a variadic function from a slice
-	return append(parsedEntries, entries...), nil
+	return contents, nil
 }
 
 func readLocalFile(inputFile string) (fileContents string, err error) {
 	f, err := os.Open(inputFile)
-	defer f.Close()
 	if err != nil {
 		//log.Printf("missing input file: %v \n", inputFile)
 		return "", fmt.Errorf("skipping missing file %s ", inputFile)
 	} else {
-		//FIXME check count, return nil if 0 maybe?
+		defer f.Close()
+		//FIXME check count, return nil if 0?
 		data := make([]byte, 2048)
 		f.Read(data)
 		return string(data), nil
