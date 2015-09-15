a4560f6b0d3d8513434fafa7623e9eb59c5208c8
George Masgras <gmasgras@gmail.com>
2015-09-14 22:58:26 -0700

    test merging playlists

--- pls_utils/pls/pls_test.go
+++ pls_utils/pls/pls_test.go
@@ -17,3 +17,29 @@ func TestAddEntry(t *testing.T) {
 	}
 
 }
+
+func TestMerge(t *testing.T) {
+	entriesPlst1 := []pls.PlaylistEntry{
+		{`http://localhost:8081`, `test1`},
+		{`http://localhost:8082`, `test2`},
+		{`http://localhost:8083`, `test3`},
+	}
+	entriesPlst2 := []pls.PlaylistEntry{
+		{`http://localhost:8084`, `test4`},
+	}
+	plst1 := &pls.Playlist{Entries: entriesPlst1}
+	plst2 := &pls.Playlist{Entries: entriesPlst2}
+
+	mergedPlaylist, _ := plst1.Merge(*plst2)
+	t.Logf("Expected %d == %d + %d", mergedPlaylist.Length(), plst1.Length(), plst2.Length())
+
+	if mergedPlaylist.Length() != plst1.Length()+plst2.Length() {
+		t.Error("Merged playlist length != sum of merged playlists")
+	}
+}
+
+func TestParse(t *testing.T) {
+}
+
+func TestMarshal(t *testing.T) {
+}
