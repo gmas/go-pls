88a4f819110e1fe2a46d61e14b031d585ce6f60b
George Masgras <gmasgras@gmail.com>
2015-09-15 22:41:50 -0700

    test parsing playlist entries test merged playlist elements

--- pls_utils/pls/pls_test.go
+++ pls_utils/pls/pls_test.go
@@ -3,6 +3,7 @@ package pls
 import (
 	"./"
 	_ "fmt"
+	"reflect"
 	"testing"
 )
 
@@ -31,15 +32,53 @@ func TestMerge(t *testing.T) {
 	plst2 := &pls.Playlist{Entries: entriesPlst2}
 
 	mergedPlaylist, _ := plst1.Merge(*plst2)
-	t.Logf("Expected %d == %d + %d", mergedPlaylist.Length(), plst1.Length(), plst2.Length())
+	t.Logf("Expected %d == %d + %d", mergedPlaylist.Length(),
+		plst1.Length(), plst2.Length())
 
 	if mergedPlaylist.Length() != plst1.Length()+plst2.Length() {
 		t.Error("Merged playlist length != sum of merged playlists")
 	}
+
+	mergedEntries := append(entriesPlst1, entriesPlst2...)
+	t.Logf("Expected %s \n==\n %s", mergedEntries, mergedPlaylist.Entries)
+	if !reflect.DeepEqual(mergedEntries, mergedPlaylist.Entries) {
+		t.Error("Merged playlist length != sum of merged playlists")
+	}
 }
 
-func TestParse(t *testing.T) {
+func TestParsePlaylistEntries(t *testing.T) {
+	playlistContent := `
+[playlist]
+numberofentries=3
+File1=http://xstream1.somafm.com:8388
+Title1=SomaFM: Beat Blender (#1 128k mp3): A late night blend of deep-house and downtempo chill.
+Length1=-1
+File2=http://xstream1.somafm.com:8384
+Title2=SomaFM: Beat Blender (#2 128k mp3): A late night blend of deep-house and downtempo chill.
+Length2=-1
+File3=http://dcstream1.somafm.com:8384
+Title3=SomaFM: Beat Blender (#3 128k mp3): A late night blend of deep-house and downtempo chill.
+Length3=-1
+`
+	parsedPlaylist, err := pls.Parse(playlistContent)
+	if err != nil {
+		t.Error("Coould not parse playlist")
+	}
+
+	testEntries := []pls.PlaylistEntry{
+		{File: `http://xstream1.somafm.com:8388`,
+			Title: `SomaFM: Beat Blender (#1 128k mp3): A late night blend of deep-house and downtempo chill.`},
+		{File: `http://xstream1.somafm.com:8384`,
+			Title: `SomaFM: Beat Blender (#2 128k mp3): A late night blend of deep-house and downtempo chill.`},
+		{File: `http://dcstream1.somafm.com:8384`,
+			Title: `SomaFM: Beat Blender (#3 128k mp3): A late night blend of deep-house and downtempo chill.`},
+	}
+	if !reflect.DeepEqual(testEntries, parsedPlaylist.Entries) {
+		t.Error("Failed to parse playlist entries correctly")
+	}
 }
+
+func TestParseVersion(t *testing.T) {}
 
 func TestMarshal(t *testing.T) {
 }
