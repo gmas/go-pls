16a18c4abd2d150d3b4750b1b89995c014f7b286
George Masgras <gmasgras@gmail.com>
2015-10-02 16:10:53 -0700

    update plsutil.go to use io.Reader

--- utils/plsutil.go
+++ utils/plsutil.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gmas/go-pls"
+	"io"
 	"log"
 	"os"
 )
@@ -12,7 +14,6 @@ func main() {
 
 	argsLen := len(os.Args)
 
-	//FIXME check arg1 for input, arg2 for output
 	if argsLen < 2 {
 		os.Exit(1)
 	}
@@ -21,14 +22,15 @@ func main() {
 	//outFile := os.Args[argsLen-1]
 
 	for _, inputFile := range inputs {
-		//TODO make it work with io.Reader
-		contents, err := loadPls(inputFile)
+
+		plsReader, err := openPls(inputFile)
 		if err != nil {
 			log.Printf("WARNING\t %s", err)
 			continue
 		}
+		log.Printf("opened file\t %s", inputFile)
 
-		playlist, err := pls.Parse(contents)
+		playlist, err := pls.Parse(plsReader)
 		//FIXME extract the error handling for warnings/errors
 		if err != nil {
 			log.Printf("WARNING\t %s", err)
@@ -44,39 +46,22 @@ func main() {
 		panic(err)
 	}
 
-	plsContent, err := newPls.Marshal()
-	fmt.Print(string(plsContent))
+	plsContentReader, err := newPls.Marshal()
+
+	// reads contents of io.Reader into a Bytes.buffer
+	plsContentBuff := new(bytes.Buffer)
+	plsContentBuff.ReadFrom(plsContentReader)
+
+	fmt.Print(plsContentBuff.String())
 }
 
 //TODO add option to download from URL
-func loadPls(inputPls string) (_ string, _ error) {
-	contents, _err := readLocalFile(inputPls)
-	if _err != nil {
-		return "", fmt.Errorf("%s \n", _err)
-	}
-	return contents, nil
-}
+func openPls(inputPls string) (io.Reader, error) {
+	reader, err := os.Open(inputPls)
 
-func readLocalFile(inputFile string) (fileContents string, err error) {
-	f, err := os.Open(inputFile)
 	if err != nil {
-		return "", fmt.Errorf("skipping missing file %s ", inputFile)
-	} else {
-		defer f.Close()
-		//FIXME check count, return nil if 0?
-		data := make([]byte, 2048)
-		f.Read(data)
-		return string(data), nil
+		return nil, fmt.Errorf("%s \n", err)
 	}
-	return
-}
 
-//func writeToFile(fileName string) {
-//	file, err := os.Create(fileName)
-//	defer func() {
-//		if err := file.Close(); err != nil {
-//			panic(err)
-//		}
-//	}()
-//
-//}
+	return reader, nil
+}
